b085e959786b856be1734c5df40efcfa944e40b7
SusannaV14 <susanna.valentini@studio.unibo.it>
2026-09-25 21:22:35 +0200

    aggiunte mie location su leaflet

--- js/leaflet.js
+++ js/leaflet.js
@@ -50,9 +50,41 @@ marker14.bindPopup("Leadenhall Market")
 var marker15 = L.marker([51.53068647181858, -0.1232585436779456]).addTo(map);
 marker15.bindPopup("King's Cross Station")
 
+var marker16 = L.marker([51.518556015389294, -0.20618526441926516]).addTo(map);
+marker16.bindPopup("27 St. Lukes Mews")
+
+var marker17 = L.marker([51.51531173120026, -0.20728066626958574]).addTo(map);
+marker17.bindPopup("The Travel Bookshop Gift Shop")
+
+var marker18 = L.marker([51.497834759759385, -0.174604720784802]).addTo(map);
+marker18.bindPopup("Victoria and Albert Museum")
+
+var marker19 = L.marker([51.50849462561429, -0.10916255363984603]).addTo(map);
+marker19.bindPopup("Oxo Gallery")
+
+var marker20 = L.marker([51.506439901518654, -0.09287482024161998]).addTo(map);
+marker20.bindPopup("8 Bedale Street")
+
+var marker21 = L.marker([51.51408898710483, -0.08947784537663035]).addTo(map);
+marker21.bindPopup("Montblanc Royal Exchange")
+
+var marker22 = L.marker([51.51891455225875, -0.09978219226747935]).addTo(map);
+marker22.bindPopup("St Bartholomew the Great")
+
+var marker23 = L.marker([51.473420992572684, -0.0017086615852271445]).addTo(map);
+marker23.bindPopup("Ranger's House")
+
+var marker24 = L.marker([51.50376291520743, -0.13918899041859908]).addTo(map);
+marker24.bindPopup("Lancaster House")
+
+var marker25 = L.marker([51.5262368467917, -0.13684100390969794]).addTo(map);
+marker25.bindPopup("187 North Gower Street")
+
 var markerGroup = L.featureGroup([
     marker1, marker2, marker3, marker4, marker5,
     marker6, marker7, marker8, marker9, marker10,
-    marker11, marker12, marker13, marker14, marker15
+    marker11, marker12, marker13, marker14, marker15,
+    marker16, marker17, marker18, marker19, marker20,
+    marker21, marker22, marker23,marker24, marker25
 ]);
 map.fitBounds(markerGroup.getBounds())
